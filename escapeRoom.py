593fe1fb48d13991087ed302832dd59fbb332b58
kimya buckner <kimyabuckner@gmail.com>
2020-03-08 12:28:27 -0500

    Lesson One girls who code

---
+++ escapeRoom.py
@@ -0,0 +1,12 @@
+
+
+userName = input('What is your name?\n')
+
+print("Hi " + ____ + "are you ready to play the game?\n")
+
+response = input("Type Y for yes type N for no\n")
+
+if(response == 'Y'):
+    print("")
+else:
+    
